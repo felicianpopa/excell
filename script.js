c43c0bcb79a7a68c9d619959308bced74487d11b
Felician Popa <felician.popa@freshbyteinc.com>
2016-08-12 12:23:59 +0300

    Calculations from the inputs.

--- script.js
+++ script.js
@@ -35,31 +35,29 @@ var grid = new Backgrid.Grid({
   // body: window.Backgrid.SummedColumnBody.extend({ columnsToSum: ['name', 'value'] })
 });
 
-
-
-
 $(document).ready(function(){
   $('.main-table').append(grid.render().el);
   $('.insert-column').on('click', function(){
+    runFormula();
     addData($(this));
     insertColumn();
     addFormula();
     addNamesToCells();
-    runFormula();
     incrementInputNames();
+    clearInputs();
   });
 
 });
-      
+// Insert a new column(plugin functionality).    
 function insertColumn() {
   grid.insertColumn([{
   label: "Comparabila 1",
-  name: "comp1",
+  name: "comp",
   editable: true,
-  cell: "string",
+  cell: "custom",
 }]);
 }
-
+// Add the data from the inputs to the column to be created.
 function addData(elem) {
   $('.inputs .input-holder').each(function(){
     //get the value from each input.
@@ -67,12 +65,11 @@ function addData(elem) {
     // get the index of each input.
     var elementIndex = $(this).index();
     // assign the values to the new column
-    col.models[elementIndex].attributes.comp1 = value ;
-    // Empty the inputs
-    $(this).find('input').val('');
+    col.models[elementIndex].attributes.comp = value ;
   });
 }
 
+// Add names to the new cells to be created.
 function addNamesToCells() {
   $('.new-column .inputs .input-holder').each(function(){
     var elementIndex = $(this).index();
@@ -80,65 +77,48 @@ function addNamesToCells() {
     $('.main-table tbody tr').eq(elementIndex).find('td:last').attr('data-cell-name', theName);
   });
 }
-
+// Add the formulas to the new cells to be created from the inputs.
 function addFormula() {
   $('.inputs .input-holder').each(function(){
     var elementIndex = $(this).index();
 
-    if($(this).find('input').attr('data-formula') != undefined) {
+    if($(this).find('input').attr('data-formula') !== undefined) {
       var theFormula = $(this).find('input').attr('data-formula');
       $('.main-table tbody tr').eq(elementIndex).find('td:last').attr('data-formula', theFormula);
     }
   });
 }
-
+// Run the formula from the inputs.
 function runFormula() {
-  $('.main-table tbody tr').each(function(){
-    var theElement = $(this).find('td[data-formula]')
-    var theFormula = $(this).find('td[data-formula]').attr('data-formula');
-    if(theFormula != undefined) {
-      eval(theFormula);
+  $('.inputs .input-holder').each(function(){
+    var theFormula = $(this).find('input').attr('data-formula');
+    if(theFormula !== undefined) {
+      var elIndex = $(this).index();
+      eval(theFormula+'(' + elIndex + ')');
     }
-  })
+  });
 }
-
+// Increment the input names.
 function incrementInputNames() {
   $('.inputs .input-holder').each(function(){
     var oldName = $(this).find('input').attr('name');
     var oldNumber = oldName.split('-')[2];
-    var newNumber = +oldNumber + 1
+    var newNumber = +oldNumber + 1;
     var newName = oldName.split('-')[0] + '-' + oldName.split('-')[1] + '-' + newNumber;
     $(this).find('input').attr('name',newName);
   });
 }
-
-// function function1(theElement) {
-//   console.log('running the function');
-//   var output = "";
-//   var columnId = $('tr .editor').index();
-//   if(columnId > 0) {
-//     var output =  parseInt($('td[data-cell-name="val-1-' + columnId + '"]').html()) +  parseInt($('td[data-cell-name="val-2-' + columnId + '"]').html());
-//   }
-//   else {
-//     var output =  parseInt($('td[data-cell-name="val-1-1"]').html()) +  parseInt($('td[data-cell-name="val-2-1"]').html());
-//   }
-  
-//   theElement.html(output)
-//   console.log(col)
-// }
-
+// Simplify the notatin for the inputs used in the formulas
+function setInput(inputNumber) {
+  return parseInt($('.inputs .input-holder').eq(inputNumber).find('input').val());
+}
+// Clear the input values
+function clearInputs() {
+    $('.inputs input').val('');
+}
+// Test formula
 function function1(theElement) {
-  // var rowIndex = 
-  console.log(theElement.parent().index())
-  var output = "";
-  var columnId = theElement.parent().index();
-  // if(columnId > 0) {
-  //   var output =  parseInt($('td[data-cell-name="val-1-' + columnId + '"]').html()) +  parseInt($('td[data-cell-name="val-2-' + columnId + '"]').html());
-  // }
-  // else {
-    var output =  parseInt($('td[data-cell-name="val-1-1"]').html()) +  parseInt($('td[data-cell-name="val-2-1"]').html());
-  // }
-  
-  theElement.html(output);
-  col.models[columnId].attributes.comp1= output;
+  var formula = setInput(0) + setInput(1);
+  // get each input which has a formula inside and update it's value
+  $('.inputs .input-holder').eq(theElement).find('input').val(formula);
 }
